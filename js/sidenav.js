ce7b4953cb4537d5ee18ce604b59a540b158adb7
wim_spaargaren@live.nl <wim_spaargaren@live.nl>
2017-06-13 17:57:22 +0200

    Menu fully inits with active working when all id's are set.

--- js/sidenav.js
+++ js/sidenav.js
@@ -9,3 +9,73 @@ function openNav() {
             document.getElementById("menubuttondiv").style.width = "40px";
       }
 }
+
+function initMenu() {
+      var menu = $("#mySidenav")[0];
+      console.log(menu);
+      var dims = JSON.parse(JSON.stringify(masterninja));
+      dims = dims.dimensions;
+      console.log(dims);
+      for (var i = 0; i < dims.length; i++) {
+            addDim("dim", dims[i].name, dims[i].linkid, menu);
+            var curSubs = dims[i].subcategories;
+            for (var j = 0; j < curSubs.length; j++) {
+                  addDim("sub", curSubs[j].name, curSubs[j].linkid, menu);
+                  var curTools = curSubs[j].tools;
+                  for (var k = 0; k < curTools.length; k++) {
+                        addDim("tool", curTools[k].name, curTools[k].linkid, menu);
+
+                  }
+            }
+      }
+}
+
+function addDim(className, name, link, menu) {
+      var div = document.createElement("div");
+      div.className = className;
+      var a = document.createElement("a");
+      a.href = "#" + link;
+      a.innerHTML = name;
+      div.appendChild(a);
+      menu.append(div)
+}
+
+
+$(document).ready(function () {
+      initMenu();
+      $(document).on("scroll", onScroll);
+      $('a[href^="#"]').on('click', function (e) {
+            e.preventDefault();
+            $(document).off("scroll");
+
+            $('a').each(function () {
+                  $(this).removeClass('active');
+            })
+            $(this).addClass('active');
+
+            var target = this.hash,
+                  menu = target;
+            $target = $(target);
+            $('html, body').stop().animate({
+                  'scrollTop': $target.offset().top + 2
+            }, 500, 'swing', function () {
+                  window.location.hash = target;
+                  $(document).on("scroll", onScroll);
+            });
+      });
+});
+
+function onScroll(event) {
+      var scrollPos = $(document).scrollTop();
+      $('#mySidenav a').each(function () {
+            var currLink = $(this);
+            var refElement = $(currLink.attr("href"));
+            if (refElement.position().top <= scrollPos && refElement.position().top + refElement.height() > scrollPos) {
+                  $('#menu-center ul li a').removeClass("active");
+                  currLink.addClass("active");
+            }
+            else {
+                  currLink.removeClass("active");
+            }
+      });
+}
