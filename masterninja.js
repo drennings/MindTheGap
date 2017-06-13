9f285540550fa56edfd9346c746314ab07f51e1d
Wim Spaargaren <wim@weave.nl>
2017-06-13 11:14:42 +0200

    ready for logos

--- masterninja.js
+++ masterninja.js
@@ -4,46 +4,54 @@ var masterninja;
 masterninja = {
   "dimensions": [{
     "name": "Coordination",
+    "backgroundlogo": "images/logos/mywonderfulllogo.png",
     "linkid": "test",
     "description": "dim desc",
     "subcategories": [
       {
         "name": "Structure",
+        "backgroundlogo": "images/logos/mywonderfulllogo.png",
         "linkid": "test",
         "description": "subdesc",
         "tools": [{
           "name": "SonarQube",
           "array": [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 10, 30, 50, 50, 50],
           "description": "Code analysis tool",
+          "backgroundlogo": "images/logos/mywonderfulllogo.png",
           "linkid": "sonarqube"
         },
         {
           "name": "Codefactor",
           "array": [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 10, 30, 50, 50, 50],
           "description": "Code analysis tool",
+          "backgroundlogo": "images/logos/mywonderfulllogo.png",
           "linkid": "codefactor"
         }]
       },
       {
         "name": "Dependencies",
+        "backgroundlogo": "images/logos/mywonderfulllogo.png",
         "linkid": "test",
         "description": "subdesc",
         "tools": [{
           "name": "Trello",
           "array": [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 10, 30, 50, 50, 50],
           "description": "Project mangaging tool for SCRUM & Agile",
+          "backgroundlogo": "images/logos/mywonderfulllogo.png",
           "linkid": "coordination_dependencies_trello"
         },
         {
           "name": "Github",
           "array": [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 10, 30, 50, 50, 50],
           "description": "Version control platform tool",
+          "backgroundlogo": "images/logos/mywonderfulllogo.png",
           "linkid": "coordination_dependencies_github"
         }]
       }
       ,
       {
         "name": "Documentation",
+        "backgroundlogo": "images/logos/mywonderfulllogo.png",
         "linkid": "test",
         "description": "subdesc",
         "tools": [{
@@ -51,17 +59,20 @@ masterninja = {
           "name": "Github Wiki",
           "array": [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 10, 30, 50, 50, 50],
           "description": "Project documentation management tool",
+          "backgroundlogo": "images/logos/mywonderfulllogo.png",
           "linkid": "github_wiki"
         },
         {
           "name": "ReadTheDocs",
           "array": [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 10, 30, 50, 50, 50],
           "description": "Project documentation manegement tool",
+          "backgroundlogo": "images/logos/mywonderfulllogo.png",
           "linkid": "readthedocs"
         }]
       },
       {
         "name": "Progress",
+        "backgroundlogo": "images/logos/mywonderfulllogo.png",
         "linkid": "test",
         "description": "subdesc",
         "tools": [{
@@ -69,12 +80,14 @@ masterninja = {
           "name": "Github",
           "array": [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 10, 30, 50, 50, 50],
           "description": "Project mangaging tool for SCRUM & Agile",
+          "backgroundlogo": "images/logos/mywonderfulllogo.png",
           "linkid": "coordination_progress_github"
         },
         {
           "name": "Palantir",
           "array": [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 10, 30, 50, 50, 50],
           "description": "Software development conflict detection tool",
+          "backgroundlogo": "images/logos/mywonderfulllogo.png",
           "linkid": "palantir"
         }]
       }]
@@ -82,11 +95,13 @@ masterninja = {
     ,
   {
     "name": "Communication",
+    "backgroundlogo": "images/logos/mywonderfulllogo.png",
     "linkid": "test",
     "description": "dim desc",
     "subcategories": [
       {
         "name": "Sharing",
+        "backgroundlogo": "images/logos/mywonderfulllogo.png",
         "linkid": "test",
         "description": "subdesc",
         "tools": [
@@ -94,23 +109,27 @@ masterninja = {
             "name": "InVision",
             "array": [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 10, 30, 50, 50, 50],
             "description": 'A web-based (and mobile) app that lets designers/developers turn their work into active prototypes with animations. Clients can provide feedback and comments directly on the site and track progress in real-time by looking at your to-do list.',
+            "backgroundlogo": "images/logos/mywonderfulllogo.png",
             "linkid": "invision"
           },
           {
             "name": "ConceptShare",
             "array": [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 10, 30, 50, 50, 50],
             "description": "A communication tool especially designed for creative types. It helps you streamline the review and approval process for any project, and it allows for online proofing with workflow automation, which means you can work with clients from one place without having to send a hundred emails back and forth.",
+            "backgroundlogo": "images/logos/mywonderfulllogo.png",
             "linkid": "conceptshare"
           },
           {
             "name": "Screen hero",
             "array": [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 10, 30, 50, 50, 50],
             "description": "Screen sharing software that works well in combination with Slack. Screen Hero allows members of the team to share their screen in real time, and to allow multiple cursors so you can actually pair program on someone else’s machine.",
+            "backgroundlogo": "images/logos/mywonderfulllogo.png",
             "linkid": "screenhero"
           }]
       },
       {
         "name": "Freely",
+        "backgroundlogo": "images/logos/mywonderfulllogo.png",
         "linkid": "test",
         "description": "subdesc",
         "tools": [
@@ -118,17 +137,20 @@ masterninja = {
             "name": "Slack",
             "array": [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 10, 30, 50, 50, 150],
             "description": "A popular communication tool, and for good reason. It takes the best of email and live-chat features and combines them into one app that you can use on your desktop or mobile device. Aside from private messages you can also create collaborative boards around different projects or topics, which is helpful if you’re working with a larger team of people or on multiple projects with a single client at once.",
+            "backgroundlogo": "images/logos/mywonderfulllogo.png",
             "linkid": "slack"
           },
           {
             "name": "Skype",
             "array": [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 10, 30, 50, 50, 150],
             "description": "An instant messaging app that provides online text message and audio/video chat services, but also has more advanced features that can be used to share files and communicate with one another",
+            "backgroundlogo": "images/logos/mywonderfulllogo.png",
             "linkid": "skype"
           }]
       },
       {
         "name": "Todo's",
+        "backgroundlogo": "images/logos/mywonderfulllogo.png",
         "linkid": "test",
         "description": "subdesc",
         "tools": [
@@ -136,12 +158,14 @@ masterninja = {
             "name": "Asana",
             "array": [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 10, 30, 50, 50, 100],
             "description": "A free project management tool designed to help teams track projects from start to finish. One of its best features is that it lets you see the overall progress of your project step by step, so your clients know exactly how long something will take without having to bug you about it.",
+            "backgroundlogo": "images/logos/mywonderfulllogo.png",
             "linkid": "asana"
           },
           {
             "name": "Wunderlist",
             "array": [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 10, 30, 50, 50, 50],
             "description": "A to-do list tool that helps you plan your individual tasks, set notifications and reminders, and collaborate with clients so they can see exactly what you’re up to and when you’re working. It is available on both desktop and mobile so you can update it on the go. If you have clients who want to see the project laid out in detail, this will help.",
+            "backgroundlogo": "images/logos/mywonderfulllogo.png",
             "linkid": "wunderlist"
           }]
       }]
@@ -149,11 +173,13 @@ masterninja = {
     ,
   {
     "name": "Cultural",
+    "backgroundlogo": "images/logos/mywonderfulllogo.png",
     "linkid": "test",
     "description": "dim desc",
     "subcategories": [
       {
         "name": "Managing",
+        "backgroundlogo": "images/logos/mywonderfulllogo.png",
         "linkid": "test",
         "description": "subdesc",
         "tools": [
@@ -161,59 +187,69 @@ masterninja = {
             "name": "GlobeSmart",
             "array": [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 10, 30, 50, 50, 50],
             "description": "Managing tool for cultural teams",
+            "backgroundlogo": "images/logos/mywonderfulllogo.png",
             "linkid": "globesmart"
           },
           {
             "name": "Git",
             "array": [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 10, 30, 50, 50, 50],
             "description": "Version control tool",
+            "backgroundlogo": "images/logos/mywonderfulllogo.png",
             "linkid": "git"
           },
           {
             "name": "GitHub",
             "array": [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 10, 30, 50, 50, 50],
             "description": "Version control platform tool",
+            "backgroundlogo": "images/logos/mywonderfulllogo.png",
             "linkid": "github"
           },
           {
             "name": "Trello",
             "array": [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 10, 30, 50, 50, 50],
             "description": "Project mangaging tool for SCRUM & Agile",
+            "backgroundlogo": "images/logos/mywonderfulllogo.png",
             "linkid": "trello"
           },
           {
             "name": "Jira",
             "array": [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 10, 30, 50, 50, 50],
             "description": "Project mangaging tool for Agile",
+            "backgroundlogo": "images/logos/mywonderfulllogo.png",
             "linkid": "jira"
           },
           {
             "name": "Slack",
             "array": [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 10, 30, 50, 50, 50],
             "description": "Chat tool",
+            "backgroundlogo": "images/logos/mywonderfulllogo.png",
             "linkid": "slack"
           },
           {
             "name": "Skype",
             "array": [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 10, 30, 50, 50, 50],
             "description": "Videocall tool",
+            "backgroundlogo": "images/logos/mywonderfulllogo.png",
             "linkid": "skype"
           }]
       },
       {
         "name": "Staff",
+        "backgroundlogo": "images/logos/mywonderfulllogo.png",
         "linkid": "test",
         "description": "subdesc",
         "tools": [
-        {
-          "name":"Linkedin",
-          "array": [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 10, 30, 50, 50, 50],
-          "description": "Business- and employment-oriented social networking service",
-          "linkid": "linkedin"
-        }]
+          {
+            "name": "Linkedin",
+            "array": [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 10, 30, 50, 50, 50],
+            "description": "Business- and employment-oriented social networking service",
+            "backgroundlogo": "images/logos/mywonderfulllogo.png",
+            "linkid": "linkedin"
+          }]
       },
       {
         "name": "Training",
+        "backgroundlogo": "images/logos/mywonderfulllogo.png",
         "linkid": "test",
         "description": "subdesc",
         "tools": [
@@ -221,6 +257,7 @@ masterninja = {
             "name": "duolingo",
             "array": [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 10, 30, 50, 50, 50],
             "description": "Language learning tool",
+            "backgroundlogo": "images/logos/mywonderfulllogo.png",
             "linkid": "duolingo"
           }]
       }]
@@ -228,11 +265,13 @@ masterninja = {
     ,
   {
     "name": "Team bonding",
+    "backgroundlogo": "images/logos/mywonderfulllogo.png",
     "linkid": "test",
     "description": "dim desc",
     "subcategories": [
       {
         "name": "External",
+        "backgroundlogo": "images/logos/mywonderfulllogo.png",
         "linkid": "test",
         "description": "subdesc",
         "tools": [
@@ -240,11 +279,13 @@ masterninja = {
             "name": "Ariadne",
             "array": [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 10, 30, 50, 50, 50],
             "description": "A tool for fostering external social capital through a digital library of knowledge called a Knowledge Pool System (KPS).",
+            "backgroundlogo": "images/logos/mywonderfulllogo.png",
             "linkid": "Ariadne"
           }]
       },
       {
         "name": "Internal",
+        "backgroundlogo": "images/logos/mywonderfulllogo.png",
         "linkid": "test",
         "description": "subdesc",
         "tools": [
@@ -252,6 +293,7 @@ masterninja = {
             "name": "WIP",
             "array": [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 10, 30, 50, 50, 50],
             "description": "This is a work in progress.",
+            "backgroundlogo": "images/logos/mywonderfulllogo.png",
             "linkid": "WIP"
           }]
       }]
